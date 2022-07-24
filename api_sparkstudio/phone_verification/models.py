ae407b3273adefa5b308bebd9241b6cbd194a7e9
Salmanul Faris <safwansalman786@gmail.com>
2022-07-24 07:57:47 +0530

    created models

--- api_sparkstudio/phone_verification/models.py
+++ api_sparkstudio/phone_verification/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    phone_regex = RegexValidator(regex=r'^1?\d{9,15}$', message="Invalid Phone Number")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=False, null=True) 
+    def __str__(self):
+        return str(self.username)
